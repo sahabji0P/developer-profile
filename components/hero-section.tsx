ea369ece264533e36396a4e0407d92722383f694
Shashwat Jain <kcapshashwat@gmail.com>
2025-02-27 23:02:01 +0530

    Adjustments in the hero section

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -26,17 +26,24 @@ export function HeroSection() {
           alt={heroData.name}
           width={200}
           height={200}
-          className="rounded-full"
+          className="rounded-full hover:shadow-lg hover:shadow-foreground/20"
         />
       </motion.div>
       <div className="text-center md:text-left">
-        <h1 className="text-3xl md:text-4xl font-bold mb-2">
-          {greeting}, I&apos;m {heroData.name}
+        <h1 className="text-3xl md:text-4xl font-bold mb-1">
+          {greeting}, I&apos;m
         </h1>
+        <motion.p
+          whileHover={{ scale: 1.05 }} transition={{ type: "spring", stiffness: 300 }}
+          className="text-primary text-3xl md:text-6xl font-bold mb-2">
+          {heroData.name}
+        </motion.p>
+
         <p className="text-muted-foreground font-semibold pb-4">@sahabji0P</p>
+
         <div className="flex flex-wrap gap-2 justify-center md:justify-start mb-4">
           {heroData.tagline.split(" | ").map((tag, index) => (
-            <Badge key={index} className="text-sm md:text-xsm font-semibold bg-secondary-foreground text-secondary">
+            <Badge key={index} className="text-sm md:text-xsm font-normal" variant="outline">
               {tag}
             </Badge>
           ))}
@@ -44,7 +51,7 @@ export function HeroSection() {
         <div className="flex items-center gap-4">
           <div className="flex items-center gap-2">
             <MapPin className="size-5 text-primary" />
-            <span className="text- text-white">Khekra, Uttar Pradesh, India</span>
+            <span className="text- text-white">{heroData.location}</span>
           </div>
           <Button
             variant="outline"
--- data/hero.ts
+++ data/hero.ts
@@ -1,7 +1,7 @@
 export const heroData = {
   name: "Shashwat Jain",
   tagline: "Engineer | Developer | Entrepreneur | Contributor",
-  location: "Khekra, U.P, India",
+  location: "Khekra, Uttar Pradesh, India",
   image:
     "/sj.jpeg"
 }
