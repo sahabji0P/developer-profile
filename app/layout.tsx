1827706e3eb86ed16714a1e46f334b62707b3718
Shashwat Jain <kcapshashwat@gmail.com>
2025-05-26 09:41:54 +0530

    edited the metadata for the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,41 @@ const roboto = Roboto({ subsets: ["latin"], weight: "400", display: "swap" })
 
 export const metadata = {
   title: "Shashwat Jain | Engineer | Developer | Entrepreneur | Contributor",
-  description: "Full-stack developer and tech enthusiast. Explore my portfolio showcasing journey, read technical/non-technical journal.",
-  keywords: "Shashwat Jain, developer, portfolio, blog, resume",
+  description: "Full-stack developer, AI/ML enthusiast, and entrepreneur crafting innovative solutions. From technical innovations, explore my diverse journey through technology, and community contributions.",
+  keywords: ["Shashwat Jain", "developer", "portfolio", "blog", "resume", "AI", "ML", "Entrepreneur", "Contributor", "Full-stack developer", "AI/ML enthusiast"],
+  authors: [{ name: "Shashwat Jain", url: "https://shashwatjain.vercel.app" }],
+
+  openGraph: {
+    title: "Shashwat Jain | Engineer | Developer | Entrepreneur | Contributor",
+    description: "Full-stack developer, AI/ML enthusiast, and entrepreneur crafting innovative solutions. From technical innovations, explore my diverse journey through technology, and community contributions.",
+    url: "https://shashwatjain.vercel.app",
+    locale: "en_US",
+    type: "website",
+    siteName: "Shashwat Jain",
+    localeAlternates: ["en_US"],
+    images: [
+      {
+        url: "https://drive.google.com/file/d/1GbHsHikZXcSJt2U2FyiBUiRd0pHJ0vI_/view?usp=sharing",
+        width: 1200,
+        height: 630,
+        alt: "Shashwat Jain | Engineer | Developer | Entrepreneur | Contributor",
+
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Shashwat Jain | Engineer | Developer | Entrepreneur | Contributor",
+    description: "Full-stack developer, AI/ML enthusiast, and entrepreneur crafting innovative solutions. From technical innovations, explore my diverse journey through technology, and community contributions.",
+    images: ["https://drive.google.com/file/d/1GbHsHikZXcSJt2U2FyiBUiRd0pHJ0vI_/view?usp=sharing"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    noimageindex: true,
+    noarchive: true,
+    nosnippet: true,
+  },
 }
 
 
